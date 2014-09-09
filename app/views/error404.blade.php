454f557f91303c60530f1398c7a5b0312d1d717b
Artem Tumin <arttumin@gmail.com>
2014-09-09 13:50:58 +0400

    404

--- app/views/error404.blade.php
+++ app/views/error404.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
- <head>
-	@include('templates.default.head')
-	@yield('style')
-</head>
-<body>
-	<!--[if lt IE 7]>
-	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
+@extends(Helper::layout())
 
-	@include('templates.default.header')
+
+@section('style')
+@stop
+
+
+@section('content')
 
     <main>
         <div class="slideshow">
@@ -39,8 +32,8 @@
         </div>
     </main>
 
-	@include('templates.default.footer')
-	@include('templates.default.scripts')
-	@yield('scripts')
-</body>
-</html>
+@stop
+
+
+@section('scripts')
+@stop
