84b029f21dff89c39c79908e217515c1664efbd2
sngrl <reserved@mail.ru>
2014-09-19 15:10:31 +0400

    +++

--- app/config/site.php
+++ app/config/site.php
@@ -7,4 +7,7 @@ return array(
     ## Disable functionality of changing url "on-the-fly" for generating
     ## seo-friendly url (via URL::route('page', '...')) with right various url-segments for multilingual pages.
     'disable_url_modification' => 0,
+
+    'uploads_dir' => public_path('uploads/files'),
+
 );
