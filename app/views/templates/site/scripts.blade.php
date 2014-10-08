8217b629680d9172fc9f2810fa446c49b619c90f
sngrl <reserved@mail.ru>
2014-10-08 11:46:39 +0400

    metrika

--- app/views/templates/site/scripts.blade.php
+++ app/views/templates/site/scripts.blade.php
@@ -11,3 +11,31 @@
 
     {{ HTML::script("js/vendor/jquery.validate.min.js") }}
     {{ HTML::script("js/vendor/jquery-form.min.js") }}
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter26541456 = new Ya.Metrika({id:26541456,
+                        webvisor:true,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true});
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="//mc.yandex.ru/watch/26541456" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
