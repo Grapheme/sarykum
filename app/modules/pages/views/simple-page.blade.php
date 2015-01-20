2a6fa427fc9df3bb5e896b7063970116e1ccf991
sngrl <reserved@mail.ru>
2015-01-20 14:56:11 +0300

    fix

---
+++ app/modules/pages/views/simple-page.blade.php
@@ -0,0 +1,42 @@
+@extends(Helper::layout())
+
+
+@if (@is_object($page->meta->seo))
+@section('title'){{ $page->meta->seo->title }}@stop
+@section('description'){{ $page->meta->seo->description }}@stop
+@section('keywords'){{ $page->meta->seo->keywords }}@stop
+@else
+@section('title')
+    {{{ $page->meta->title }}}@stop
+@section('description')
+    {{{ striptags($page->meta->preview) }}}@stop
+@endif
+
+@section('style')
+@stop
+
+
+@section('content')
+    <main>
+        <h1>
+            {{ @is_object($page->meta->seo) && $page->meta->seo->h1 ? $page->meta->seo->h1 : $page->title }}
+        </h1>
+
+        @if (@is_object($page->blocks) && $page->blocks->count())
+            <div>
+                {{ Helper::ta_($page->blocks) }}
+                @foreach($page->blocks as $block)
+                    @if (is_object($block->meta))
+                        {{ $block->meta->content }}
+                    @endif
+                @endforeach
+            </div>
+            <hr />
+        @endif
+
+    </main>
+@stop
+
+
+@section('scripts')
+@stop
