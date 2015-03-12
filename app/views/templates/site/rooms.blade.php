2d974e9a0f13c9743c31e0d70eb053e0e6078061
sngrl <reserved@mail.ru>
2015-03-12 16:39:28 +0300

    fix

--- app/views/templates/site/rooms.blade.php
+++ app/views/templates/site/rooms.blade.php
@@ -1,18 +1,18 @@
 <?
 $rooms = Dic::whereSlugValues('room_type');
 if (@count($rooms))
-    foreach ($rooms as $r => $room) {
-        #Helper::tad($room);
-        if (count($room->fields)) {
-            foreach ($room->fields as $field) {
-                $room->{$field->key} = $field->value;
-            }
-            unset($room->fields);
-        }
-        if ($room->slug)
-            $rooms[$room->slug] = $room;
-        unset($rooms[$r]);
-    }
+foreach ($rooms as $r => $room) {
+#Helper::tad($room);
+if (count($room->fields)) {
+foreach ($room->fields as $field) {
+$room->{$field->key} = $field->value;
+}
+unset($room->fields);
+}
+if ($room->slug)
+$rooms[$room->slug] = $room;
+unset($rooms[$r]);
+}
 #Helper::tad($rooms);
 #$room_standard = Dic::valueBySlugs('room_type', 'standard');
 #$room_business = Dic::valueBySlugs('room_type', 'standard');
@@ -26,65 +26,67 @@ if (@count($rooms))
 
 @section('content')
 
-<main role="main">
-    @if (count($rooms))
-    <section class="rooms">
-        <div class="row clearfix no-padding">
-            @foreach ($rooms as $room_type => $room)
-            <?
-            $obj = $room;
-            #Helper::tad($obj->meta);
-            if (@!is_object($obj->meta) || !$obj->meta->name)
-                continue;
+    <main role="main">
+        @if (count($rooms))
+            <section class="rooms">
+                <div class="row clearfix no-padding">
+                    @foreach ($rooms as $room_type => $room)
+                        <?
+                        $obj = $room;
+                        #Helper::tad($obj->meta);
+                        if (@!is_object($obj->meta) || !$obj->meta->name)
+                        continue;
 
-            $photo = false;
-            if (@is_numeric($obj->image)) {
-                $photo = Photo::find($obj->image) ?: false;
-            }
-            ?>
-            <div class="column third">
-                <div class="room-item" style="background-image:url({{ (is_object($photo) ? $photo->full() : '') }});">
-                    <a href="{{ URL::route('room', $room->slug) }}">
-                        @if ($room->name)
-                        <div class="room-name">
-                            {{ $room->meta->name }}
+                        $photo = false;
+                        if (@is_numeric($obj->image)) {
+                        $photo = Photo::find($obj->image) ?: false;
+                        }
+                        ?>
+                        <div class="column third">
+                            <div class="room-item" style="background-image:url({{ (is_object($photo) ? $photo->full() : '') }});">
+                                <a href="{{ URL::route('room', $room->slug) }}">
+                                    @if ($room->name)
+                                        <div class="room-name">
+                                            {{ $room->meta->name }}
+                                        </div>
+                                    @endif
+                                    @if ($room->price)
+                                        <div class="room-price room-price-1">
+                                            <span class="price-num">{{ $room->price }}</span>
+                                            {{ trans("interface.rooms.single_occupancy") }}
+                                        </div>
+                                    @endif
+                                    @if ($room->price2)
+                                        <div class="room-price room-price-2">
+                                            <span class="price-num">{{ $room->price2 }}</span>
+                                            {{ trans("interface.rooms.double_occupancy") }}
+                                        </div>
+                                    @endif
+                                </a>
+                            </div>
                         </div>
-                        @endif
-                        @if ($room->price)
-                        <div class="room-price room-price-1">
-                            <span class="price-num">{{ $room->price }}</span>
-                            {{ trans("interface.rooms.single_occupancy") }}
-                        </div>
-                        @endif
-                        @if ($room->price2)
-                        <div class="room-price room-price-2">
-                            <span class="price-num">{{ $room->price2 }}</span>
-                            {{ trans("interface.rooms.double_occupancy") }}
-                        </div>
-                        @endif
-                    </a>
+                    @endforeach
                 </div>
-            </div>
-            @endforeach
-        </div>
-    </section>
-    @endif
+            </section>
+        @endif
 
-    {{ is_object($page->meta) && is_object($page->meta->seo) ? '<h1 class="text-center">' . $page->meta->seo->h1 . '</h1>' : '' }}
 
-    {{ $page->block('content') }}
+        <section class="sect-wrap rooms-wrap">
+            {{ is_object($page->meta) && is_object($page->meta->seo) ? '<h1 class="text-center">' . $page->meta->seo->h1 . '</h1>' : '' }}
+            {{ $page->block('content') }}
+        </section>
 
-    <section class="sect-wrap">
-        <p>
-        <button class="btn btn-primary reserve_room">
-            <span class="icon icon-booking"></span> {{ trans('interface.menu.reserve') }}
-        </button>
-        </p>
-    </section>
+        <section class="sect-wrap">
+            <p>
+                <button class="btn btn-primary reserve_room">
+                    <span class="icon icon-booking"></span> {{ trans('interface.menu.reserve') }}
+                </button>
+            </p>
+        </section>
 
-    {{ $page->block('seo') }}
+        {{ $page->block('seo') }}
 
-</main>
+    </main>
 @stop
 
 
